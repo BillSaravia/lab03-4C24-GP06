d402df7f7b2ad9732e1739bcbec5be9069e022b4
ErikaVentura <erika.ventura@tecsup.edu.pe>
2022-09-05 18:11:14 -0500

    Clases

--- src/main/java/com/tecsup/lab3/repository/StudentRepository.java
+++ src/main/java/com/tecsup/lab3/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.tecsup.lab3.repository;
 
 public class StudentRepository {
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	    }
+	
 
 }
--- src/main/java/com/tecsup/lab3/view/StudentReport.java
+++ src/main/java/com/tecsup/lab3/view/StudentReport.java
@@ -1,5 +1,11 @@
 package com.tecsup.lab3.view;
 
 public class StudentReport {
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	    }
+	
 
 }
